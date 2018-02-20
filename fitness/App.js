d490100040f36a0e338ee076ccf8920cff15e55a
catdog990 <ozmcastro@gmail.com>
2018-02-20 15:38:49 -0500

    userAuth

--- fitness/App.js
+++ fitness/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text, View, TextInput } from 'react-native';
 
+
+
 export default class userInput extends Component {
   constructor(props){
     super(props);
@@ -10,21 +12,28 @@ export default class userInput extends Component {
   render(){
 
     return(
- 
 <View>
 
- 
+   <View style={{ 
+        flex: 3,
+        flexDirection: 'column',
+        
+        alignItems: 'center'
+        }}>
+
       <TextInput
-      style={{height: 40, flex: 1, alignItems: 'center'}}
+      style={{height: 50, width: 150}}
       placeholder="Type Username"
       onChangeText={(text) => this.setState({text})}
     />
-   
- 
+      <TextInput
+      style={{height: 50, width: 150}}
+      placeholder="Type Password"
+      onChangeText={(text) => this.setState({text})}
+    />
+    </View>
+  
 </View>
-   
-
-
   
     );
 
