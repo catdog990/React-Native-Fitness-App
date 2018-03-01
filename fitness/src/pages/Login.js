21b0d7edf86d03b5b5b33bdebe41b47612c0b53e
catdog990 <ozmcastro@gmail.com>
2018-03-01 04:29:14 -0500

    adding more to auth.

--- fitness/src/pages/Login.js
+++ fitness/src/pages/Login.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
-import {  Text, View, TextInput, StyleSheet } from 'react-native';
+import {  Text, View, TextInput, StyleSheet, Button } from 'react-native';
 import Verification from '../components/verification';
 import Logo from '../components/logo';
+import Amplify, { Auth } from 'aws-amplify'
 
 
 
-export default class Login extends Component {
+export default class Signup extends Component {
+
+  state= {
+    username: '',
+    password: '',
+    user: {},
+    confirmationNumber: ''
+  }
+
+  onChangeText(key, value){
+    this.setState({
+      [key]: value
+    })
+  }
+
+  signIn(){
+    const {username, password} = this.state
+    Auth.signIn(
+      username, 
+      password
+    )
+      .then(() => console.log("Logged In!"))
+      .catch(err => console.log('Error Logging In!:', err))
+  }
+
+  confirmSignUp(){
+    Auth.confirmSignUp(
+      this.state.username,
+      this.state.confirmationNumber
+    )
+    .then(() => console.log("Confirmed!"))
+    .catch(err => console.log('Error with Confirmation!:', err))
+  }
 
   render(){
 
@@ -13,11 +46,42 @@ export default class Login extends Component {
         <View style={styles.container}>
         
             <Logo />
-            <Verification />
-          
-          <View style={styles.question}>
+         
+
+            <TextInput 
+              onChangeText={value => this.onChangeText('username', value)}
+              style={styles.userInput}
+              placeholder='Username'
+              placeholderTextColor="white"
+              underlineColorAndroid='rgba(0,0,0,0)'
+            />
+            <TextInput 
+              onChangeText={value => this.onChangeText('password', value)}
+              style={styles.userInput}
+              placeholder='Password'
+              secureTextEntry={true}
+              placeholderTextColor="white"
+              underlineColorAndroid='rgba(0,0,0,0)'
+            />
+
+            <Button title="Log In" onPress={this.signUp.bind(this)}/>
+
+            <TextInput 
+              onChangeText={value => this.onChangeText('confirmationNumber', value)}
+              style={styles.userInput}
+              placeholder='Confirmation Number'
+              placeholderTextColor="white"
+              underlineColorAndroid='rgba(0,0,0,0)'
+            />
+
+            <Button title="Confirm" onPress={this.confirmSignUp.bind(this)}/>
+
+        
+
+
+             <View style={styles.question}>
             <Text style={styles.textQ}>Don't have an account?</Text>
-            <Text style={styles.textS}>Signup!</Text>
+            <Text style={styles.textS}>Sign Up!</Text>
           </View>
           
               
@@ -30,23 +94,46 @@ export default class Login extends Component {
 };
 
 const styles = StyleSheet.create ({
-  container : {
-    backgroundColor: '#263238',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  question : {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row'
-  },
-  textQ: {
-    color: 'rgba(255, 255, 255, 0.5)'
-  },
-  textS: {
-    color: 'white',
-    paddingHorizontal: 10
-  }
-});
+    container : {
+      backgroundColor: '#263238',
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    
+    userInput : {
+      width:300,
+      backgroundColor: 'rgba(255, 255, 255, 0.3)',
+      borderRadius: 25,
+      paddingHorizontal: 16,
+      fontSize: 16,
+      color: 'white',
+      marginVertical: 10
+  
+    },
+    question : {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'row'
+    },
+    textQ: {
+      color: 'rgba(255, 255, 255, 0.5)'
+    },
+    textS: {
+      color: 'white',
+      paddingHorizontal: 10
+    },
+    touch: {
+      width:100,
+      backgroundColor: '#0087a2',
+      borderRadius: 25,
+      marginVertical: 10
+  
+    },
+    touchText: {
+      fontSize: 16,
+      color: 'white',
+      textAlign: 'center'
+    }
+  });
